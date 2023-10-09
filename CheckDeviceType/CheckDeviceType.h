511ef7052efc2211ee7611df6d28e16eefab52ac
Nonato Sousa <nonato.sousa@antikytera.com>
2023-10-09 16:54:14 -0300

    Initial Commit

---
+++ CheckDeviceType/CheckDeviceType.h
@@ -0,0 +1,18 @@
+//
+//  CheckDeviceType.h
+//  CheckDeviceType
+//
+//  Created by Nonato Sousa on 09/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckDeviceType.
+FOUNDATION_EXPORT double CheckDeviceTypeVersionNumber;
+
+//! Project version string for CheckDeviceType.
+FOUNDATION_EXPORT const unsigned char CheckDeviceTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckDeviceType/PublicHeader.h>
+
+
